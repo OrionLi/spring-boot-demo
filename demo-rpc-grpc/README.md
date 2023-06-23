4c90bfea6f256f64b400fc3163414aa41e61aa79
OrionLi <orionli@petalmail.com>
2023-06-23 16:57:24 +0800

    gRPC 完结

--- demo-rpc-grpc/README.md
+++ demo-rpc-grpc/README.md
@@ -1,5 +1,7 @@
 # gRPC
 
+# 快速开始
+
 启动nacos
 
 先启动 [`GrpcUserserviceApplication`](grpc-userservice/src/main/java/com/orion/demo/grpc/userservice/GrpcUserserviceApplication.java)
@@ -9,3 +11,27 @@
 
 浏览器访问 [`http://127.0.0.1:8080/demo/create?name=yudaoyuanma&gender=1`](http://127.0.0.1:8080/demo/create?name=yudaoyuanma&gender=1)
 ，如果能看到一个随机生成的id证明运行成功
+
+# 介绍
+
+没有用很多教程中的 第三方 grpc-starter，而是手动配置 grpc 客户端和服务端，方便学习
+
+gRPC 服务端启动完成后，会将自己的 gRPC 地址添加到元数据，服务注册到 Nacos 时会携带这些元数据
+
+gRPC 客户端会通过服务端注册到 Nacos 的名字(`spring.application.name`的值)从 Nacos 拿到对应的 gRPC服务端 地址，实现远程调用
+
+客户端那边的配置已经尽可能的封装了。添加一个新的服务端只需要按这个格式新增一个 Bean 就行了
+
+```java
+@Bean
+public UserServiceGrpc.UserServiceBlockingStub userServiceGrpc()throws NacosException{
+        return createBlockingStub("user-service",UserServiceGrpc::newBlockingStub);
+        }
+```
+
+# 官方文档传送门
+
+## 关于 `NacosDiscovery` 的
+
+[官方快速开始](https://github.com/alibaba/spring-cloud-alibaba/wiki/Nacos-discovery)
+[通过Java SDK配置](https://nacos.io/zh-cn/docs/v2/guide/user/sdk.html)
--- demo-rpc-grpc/grpc-application/src/main/java/com/orion/demo/grpc/application/config/GrpcConfig.java
+++ demo-rpc-grpc/grpc-application/src/main/java/com/orion/demo/grpc/application/config/GrpcConfig.java
@@ -27,6 +27,30 @@ public class GrpcConfig {
     @Autowired
     private NacosDiscoveryProperties discoveryProperties;
 
+    /**
+     * 如果后面还需要其他的 XxxServiceGrpc.XxxServiceBlockingStub，就像下面这个方法一样写
+     */
+    @Bean
+    public UserServiceGrpc.UserServiceBlockingStub userServiceGrpc() throws NacosException {
+        return createBlockingStub("user-service", UserServiceGrpc::newBlockingStub);
+    }
+
+    /**
+     * @param serviceName 服务名
+     * @param stubFactory 客户端 ManagedChannel 的工厂类
+     * @return XxxServiceBlockingStub 实例
+     */
+    public <T extends AbstractBlockingStub<T>> T createBlockingStub(String serviceName,
+                                                                    Function<ManagedChannel, T> stubFactory) throws NacosException {
+        ManagedChannel managedChannel = createManagedChannel(serviceName);
+        return stubFactory.apply(managedChannel);
+    }
+
+    /**
+     * 创建 ManagedChannel
+     *
+     * @param serviceName 服务名
+     */
     private ManagedChannel createManagedChannel(String serviceName) throws NacosException {
         // 获取指定服务的地址和端口
         NamingService namingService = NamingFactory.createNamingService(discoveryProperties.getServerAddr());
@@ -36,23 +60,14 @@ public class GrpcConfig {
         return ManagedChannelBuilder.forAddress(address, port).usePlaintext().build();
     }
 
-    public <T extends AbstractBlockingStub<T>> T createBlockingStub(String serviceName,
-                                                                    Function<ManagedChannel, T> stubFactory) throws NacosException {
-        ManagedChannel managedChannel = createManagedChannel(serviceName);
-        return stubFactory.apply(managedChannel);
-    }
-
-    @Bean
-    public UserServiceGrpc.UserServiceBlockingStub userServiceGrpc() throws NacosException {
-        return createBlockingStub("user-service", UserServiceGrpc::newBlockingStub);
-    }
-
-    // 获取 Service 的元数据中的 gRPC 端口号
+    /**
+     * 获取 Service 的元数据中的 gRPC 端口号
+     */
     private int getServiceGrpcPort(Instance instance) {
         String grpcPort = instance.getMetadata().get("grpcPort");
         if (StringUtils.isEmpty(grpcPort)) {
             // 当元数据中不存在 grpcPort 时，抛出异常
-            throw new RuntimeException("No grpcPort in service metadata.");
+            throw new RuntimeException("服务元数据中没有\"grpcPort\"字段");
         }
         return Integer.parseInt(grpcPort);
     }
