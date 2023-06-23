c3ee287f6e827f9dc8ac9f524448598f105beb4e
OrionLi <orionli@petalmail.com>
2023-06-23 16:30:13 +0800

    封装太难，放弃了

--- demo-rpc-grpc/grpc-application/src/main/java/com/orion/demo/grpc/application/controller/DemoController.java
+++ demo-rpc-grpc/grpc-application/src/main/java/com/orion/demo/grpc/application/controller/DemoController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @Autowired
-    //@GrpcService(serviceName = "user-service")
     private UserServiceGrpc.UserServiceBlockingStub userServiceGrpc;
 
     @GetMapping("/get")
